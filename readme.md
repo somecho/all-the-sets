a441119386473fa1513b7256701b17639b8398ec
samuel cho <samonthepiano@gmail.com>
2023-03-06 18:52:32 +0100

    add readme

---
+++ readme.md
@@ -0,0 +1,21 @@
+# All The Sets
+
+A very simple web service to provide the unique prime forms of Pitch-Class Sets
+as listed in Appendix 1 of [Allen Forte's The Structure Of Atonal
+Music](https://books.google.de/books/about/The_Structure_of_Atonal_Music.html?id=j9aV2JYHY4AC&redir_esc=y)
+as a REST api. 
+
+## Usage
+
+This service is hosted [on Render](https://all-the-sets.onrender.com): https://all-the-sets.onrender.com
+
+### API
+| / | returns the complete list of prime forms |
+| /set/:name | Where name is the classification that Allen Forte uses, for example 3-3 or 5-1 | 
+| /size/:size | Returns all the prime forms of Pitch-Class sets with a given size | 
+
+## Running locally
+After cloning this repo
+```
+npm install && node server.js
+```
--- server.js
+++ server.js
@@ -5,28 +5,39 @@ var fs = require('fs')
 var pcData = JSON.parse(fs.readFileSync("data.json"))
 let PORT = process.env.PORT || 8081
 
+//////////////////////////////
+// FUNCTIONS
+//////////////////////////////
+function pcWithSize(size){
+  let asArray = Object.entries(pcData)
+  let filtered = asArray.filter(([key, value])=> value.length == size)
+  return Object.fromEntries(filtered);
+}
 
+//////////////////////////////
+// ROUTES
+//////////////////////////////
 app.get('/', (req,res)=>{
-  // res.send('Hello World')
   res.end(JSON.stringify(pcData))
 })
 
-// app.get('/my-app',function(req,res){
-//   res.send('This is my app')
-// })
+app.get('/set/:name', (req,res)=>{
+  let name = pcData[req.params.name] ? req.params.name : "does not exist"
+  let response = {
+    name,
+    pcs: pcData[req.params.name] || []
+  }
+  res.end(JSON.stringify(response))
+})
 
-// app.get('/format',function(req,res){
-//   var response = {
-//     name: "Barbara",
-//     age: 33
-//   }
-//   res.end(JSON.stringify(response))
-// })
+app.get('/size/:size', (req,res)=>{
+  let response = pcWithSize(req.params.size)
+  res.end(JSON.stringify(response))
+})
 
-// app.get('/vars/:name', function(req,res){
-//   res.send(req.params.name)
-// })
-
+//////////////////////////////
+// START SERVER
+//////////////////////////////
 var server = app.listen(PORT, ()=>{
-  console.log(`Example app listening at port ${PORT}`)
+  console.log(`All The Sets listening at port ${PORT}`)
 })
