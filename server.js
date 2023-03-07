6d4420c6b52510fd61a01d11e593925d5f56e934
samuel cho <samonthepiano@gmail.com>
2023-03-07 08:38:03 +0100

    add new route for pcs matching

--- server.js
+++ server.js
@@ -8,10 +8,38 @@ let PORT = process.env.PORT || 8081
 //////////////////////////////
 // FUNCTIONS
 //////////////////////////////
+function arrayEquals(a,b){
+  return Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val,index)=>val===b[index])
+}
+
 function pcWithSize(size){
   let asArray = Object.entries(pcData)
   let filtered = asArray.filter(([key, value])=> value.length == size)
   return Object.fromEntries(filtered);
+}
+
+function matchPC(pcs){
+  let items = pcs.split(',');
+  let ints = items.map(i=>parseInt(i))
+  //VALIDATE
+  for(let i in ints) {
+    if(isNaN(ints[i]) || ints[i] < 0 || ints[i] > 11){
+      return {"name":"not a valid PC set"};
+    }
+  }
+  //SORT
+  ints.sort()
+  let asArray = Object.entries(pcData)
+  let result = asArray.filter(([key,value])=>arrayEquals(value,ints))
+  if (result.length == 0) {
+    return {"name":"Not a unique prime form, or not in prime form, or does not exist."}
+  } else {
+    return {"name":result[0][0],
+            "pcs":ints} //there is only one result from filter
+  }
 }
 
 //////////////////////////////
@@ -32,6 +60,11 @@ app.get('/set/:name', (req,res)=>{
 
 app.get('/size/:size', (req,res)=>{
   let response = pcWithSize(req.params.size)
+  res.end(JSON.stringify(response))
+})
+
+app.get('/pcs/:pcs',(req,res)=>{
+  let response = matchPC(req.params.pcs)
   res.end(JSON.stringify(response))
 })
 
